281f61586a7d3b7c259f3f7af373a51d8801e5d1
Christine Cion <cion.c.bscs@gmail.com>
2023-09-11 11:18:23 +0800

    added comments for notes

--- myApp.js
+++ myApp.js
@@ -1,9 +1,14 @@
 const express = require('express');
+// STEP 1: Install and Require Helmet
 const helmet = require('helmet');
 const app = express();
-
+//STEP 2: Hide Potentially Dangerous Information Using helmet.hidePoweredBy()
 app.use(helmet.hidePoweredBy());
-
+// STEP 3: Use helmet.frameguard() passing with the configuration object {action: 'deny'}
+// Clickjacking is a technique of tricking a user into interacting with a page different from what the user thinks it is. 
+// This can be obtained by executing your page in a malicious context, by means of iframing. 
+// In that context, a hacker can put a hidden layer over your page. 
+// You can also check if your app is safe from clickjacking to this site https://clickjacker.io/
 app.use(helmet.frameguard({action: 'deny'}));
 
 
@@ -59,5 +64,6 @@ app.get("/", function (request, response) {
 });
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
+  // STEP 1: edit a description in nmp start
   console.log(`Arcane Programmer Info Security App Started on Port ${port}`);
 });
